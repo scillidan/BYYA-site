f2d8141990cf3f6046f189e011850794faac7a1a
scillidan <scillidan@yandex.com>
2023-07-12 01:50:32 +0800

    Update hugo_optimized.sh

--- hugo_optimized.sh
+++ hugo_optimized.sh
@@ -1,5 +1,4 @@
 if hash exiftool 2>/dev/null; then
-	# remove exif data on all images in new_images
 	exiftool -all= public/images*
 else
 	echo "Install perl-image-exiftool to optimize images"
@@ -7,9 +6,7 @@ fi
 
 if hash jpegoptim 2>/dev/null; then
 	for image in $(find public/images -regextype posix-extended -iregex ".*\.(jpeg|jpg)"); do
-		# resize to width 1400 only if bigger than that
 		mogrify -resize '1400>' $image
-		# remove all metadata and try to optimize jpeg image to match the Maximum size defined above
 		jpegoptim --strip-all --size=$MAXIMUM_JPG_SIZE $image
 	done;
 else
@@ -18,9 +15,7 @@ fi
 
 if hash optipng 2>/dev/null; then
 	for image in $(find public/images -regextype posix-extended -iregex ".*\.(png)"); do
-		# resize to width 1400 only if bigger than that
 		mogrify -resize '1400>' $image
-		# optimize PNG with a give level (higher = slower) and remove all metadata
 		optipng -clobber -strip all -o $PNG_OPTIMIZATION_LEVEL $image
 	done;
 else
